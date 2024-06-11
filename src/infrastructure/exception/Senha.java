76dd4450b0edbfb41d9cac32d32c7cf367d2b211
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-11 14:44:32 -0300

    feat: criando metodo em senha para resgatar conteudo

--- src/infrastructure/exception/Senha.java
+++ src/infrastructure/exception/Senha.java
@@ -44,4 +44,8 @@ public class Senha {
     public void setConteudo(String conteudo){
         this.conteudo = conteudo;
     }
+
+    public String getConteudo(){
+        return conteudo;
+    }
 }
